2d962e48e9d514c0538ae7a502b8c348c882de41
pie-1 <nepali07pawan@gmail.com>
2025-07-08 00:45:00 +0545

    login page  feature

--- frontend/src/app/router.jsx
+++ frontend/src/app/router.jsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter } from 'react-router'
 import RegisterPage from '../features/auth/components/RegisterPage'
+import LoginPage from '../features/auth/components/LoginPage'
 
 export const router = createBrowserRouter([
   { path: '/', element: <div>Hello World!</div> },
   { path: '/register', element: <RegisterPage /> },
+  { path: '/login', element: <LoginPage /> },
 ])
