12196d07201415513b252220fdc47ccd0dd7ad5c
sahamdot <salahuddinahammed@yahoo.com>
2025-07-08 09:04:18 +0600

    feat(auth): implement auth layout and update register page

--- frontend/src/features/auth/components/RegisterPage.jsx
+++ frontend/src/features/auth/components/RegisterPage.jsx
@@ -1,7 +1,7 @@
 import { useForm } from 'react-hook-form'
-
 import Input from '../../../components/Input'
 import PasswordInput from '../../../components/PasswordInput'
+import AuthLayout from './AuthLayout'
 
 export default function RegisterPage() {
   const {
@@ -12,16 +12,13 @@ export default function RegisterPage() {
   const onSubmit = data => data
 
   return (
-    <div className="flex flex-col justify-center items-center h-dvh">
-      <div className="flex flex-col justify-center items-center">
-        <img src="logo.svg" className="size-12"></img>
-        <h1 className="font-display-3xl text-[#949494]">
-          Welcome to
-          {' '}
-          <span className="text-[#000000]">OpenShelf</span>
-        </h1>
-      </div>
-      <div className="w-96 mt-8">
+    <AuthLayout>
+      <AuthLayout.Header>
+        Welcome to
+        {' '}
+        <span className="text-[#000000]">OpenShelf</span>
+      </AuthLayout.Header>
+      <AuthLayout.Body>
         <form onSubmit={handleSubmit(onSubmit)}>
           <Input
             label="Display Name"
@@ -57,20 +54,14 @@ export default function RegisterPage() {
 
           <input type="submit" className="bg-lavender-500 text-white rounded w-full py-2 mt-7 cursor-pointer" value="Continue" />
 
-          <div className="mt-4 flex flex-row items-center gap-8">
-            <hr className="border-stroke-weak w-full"></hr>
-            <p className="text-[#CACACA] text-lg">Or</p>
-            <hr className="border-stroke-weak w-full"></hr>
-          </div>
+          <AuthLayout.ButtonDivider />
         </form>
 
         <button className="border-stroke-weak border-1 px-4 py-3 rounded-xl flex flex-row items-center w-full mt-4 cursor-pointer" type="submit">
           <img src="google.png" className="size-6" />
           <p className="w-full">Sign in with Google</p>
         </button>
-      </div>
-
-      <p className="text-sm text-stroke-strong absolute bottom-24">Made by Team Lavender 💜</p>
-    </div>
+      </AuthLayout.Body>
+    </AuthLayout>
   )
 }
