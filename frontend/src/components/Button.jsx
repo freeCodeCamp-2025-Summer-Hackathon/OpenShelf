2f2ed2a06dfe981fdb70ee57bf45a57b0fb7661a
unknown <enxin06@gmail.com>
2025-07-11 12:06:43 +0800

    feat(frontend): add reusable button component

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import GoogleIcon from "frontend\public\google.png";
+
+const Icon = {
+  google: GoogleIcon,
+};
+
+const Button = ({ label, variant = "primary", icon, onClick }) => {
+  const baseStyles = `
+    relative
+    cursor-pointer
+    rounded-[10px]
+    w-[400px]
+    h-[40px]
+    overflow-hidden
+    p-0
+    max-[500px]:w-[70%]
+    max-[500px]:min-w-[180px]
+  `;
+
+  const variantStyles =
+    variant === "primary"
+      ? "bg-[#575799] text-white border-none"
+      : "bg-transparent text-black border-[2px] border-white";
+
+  const iconStyles = `
+    absolute
+    left-[5px]
+    top-[50%]
+    translate-y-[-50%]
+    h-[30px]
+    aspect-[1/1]
+    object-contain
+  `;
+
+  const textStyles = `
+    absolute
+    top-[50%]
+    left-[50%]
+    translate-x-[-50%]
+    translate-y-[-50%]
+    white-space-nowrap
+    pointer-events-none
+    ${variant === "outline" ? "max-[400px]:left-[calc(50%+15px)]" : ""}
+  `;
+
+  return (
+    <button className={`${baseStyles} ${variantStyles}`} onClick={onClick}>
+      {icon && <img src={Icon[icon]} className={iconStyles} />}
+      <span className={textStyles}>{label}</span>
+    </button>
+  );
+};
+
+export default Button;
