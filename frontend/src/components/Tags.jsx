8f37b66b2f3c7225fe23906ff065c28d163b3ffb
CatMCGT <104284630+CatMCGT@users.noreply.github.com>
2025-07-20 16:20:32 +0800

    fix(frontend): use clsx utility to conditionally apply classnames

--- frontend/src/components/Tags.jsx
+++ frontend/src/components/Tags.jsx
@@ -1,13 +1,15 @@
-function Tags({ label, type }) {
-  const tagStyles = {
-    condition: 'bg-lavender-200',
-    category: 'bg-[#FFD3D3]',
-    genre: 'bg-[#D3D3F1]',
-  }
+import clsx from 'clsx'
 
-  const typeClass = tagStyles[type]
+function Tags({ label, type }) {
   return (
-    <span className={`${typeClass} font-sans-sm py-1 px-3 rounded-md`}>
+    <span
+      className={clsx(
+        'bg-lavender-200' && type === 'condition',
+        'bg-[#FFD3D3]' && type === 'category',
+        'bg-[#D3D3F1]' && type === 'genre',
+        'font-sans-sm py-1 px-3 rounded-md'
+      )}
+    >
       {label}
     </span>
   )
