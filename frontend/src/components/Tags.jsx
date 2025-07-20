0465c9f036e2471b528574afd40894be01902057
CatMCGT <104284630+CatMCGT@users.noreply.github.com>
2025-07-20 16:36:33 +0800

    fix(frontend): fix clsx utility for Tags

--- frontend/src/components/Tags.jsx
+++ frontend/src/components/Tags.jsx
@@ -1,12 +1,14 @@
 import clsx from 'clsx'
 
-function Tags({ label, type }) {
+function Tags({ type, label }) {
   return (
     <span
       className={clsx(
-        'bg-lavender-200' && type === 'condition',
-        'bg-[#FFD3D3]' && type === 'category',
-        'bg-[#D3D3F1]' && type === 'genre',
+        {
+          'bg-lavender-200': type == 'condition',
+          'bg-[#FFD3D3]': type == 'category',
+          'bg-[#D3D3F1]': type == 'genre',
+        },
         'font-sans-sm py-1 px-3 rounded-md'
       )}
     >
